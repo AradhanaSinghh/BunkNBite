f99d01909aeda4657ebf38e8c1f1b7d2eaad243b
Your name <aradhanaswan3456@gmail.com>
2026-08-31 15:26:35 +0530

    created frontend with api intergration from backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,16 @@
 const express=require('express');
 const cookieParser=require('cookie-parser');
+const cors=require('cors');
 
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 const authRoutes=require("./routes/auth.routes.js");
+const foodRoutes=require("./routes/food.routes.js");
 
 app.get("/",(req,res)=>{
     res.send("hello world!!")
@@ -13,4 +18,5 @@ app.get("/",(req,res)=>{
 
 app.use('/api/auth',authRoutes);
 
+app.use('/api/food',foodRoutes);
 module.exports=app;
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,8 +3,15 @@ const authController=require("../controllers/auth.controller.js");
 
 const router=express.Router();
 
+//user auth api's 
 router.post('/register',authController.registerUser);
 router.post('/login',authController.loginUser);
 router.get('/logout',authController.logoutUser);
+
+
+//food Partner auth api's
+router.post('/food-partner/register',authController.registerFoodPartner);
+router.post('/food-partner/login',authController.loginFoodPartner);
+router.get('/food-partner/logout',authController.logoutFoodPartner);
 
 module.exports=router;
